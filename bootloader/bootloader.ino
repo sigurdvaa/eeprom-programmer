72a02c7c28ee6b56498413742195a92d043d58b3
Sigurd Våg Aaknes <sigurdvaa@gmail.com>
2023-02-02 00:11:52 +0100

    init stability test

---
+++ bootloader/bootloader.ino
@@ -0,0 +1,183 @@
+/**
+ * This sketch programs the microcode EEPROMs for the 8-bit breadboard computer
+ * It includes support for a flags register with carry and zero flags
+ * See this video for more: https://youtu.be/Zg1NdPKoosU
+ */
+#define SHIFT_DATA 2
+#define SHIFT_CLK 3
+#define SHIFT_LATCH 4
+#define EEPROM_D0 5
+#define EEPROM_D7 12
+#define WRITE_EN 13
+#define EEPROM_SIZE 2048
+#define RAM_SIZE 256
+
+
+#define BOOT 0
+#define LDA 1
+#define ADD 2
+#define SUB 3
+#define STA 4
+#define LDI 5
+#define JMP 6
+#define JC 7
+#define JZ 8
+
+#define OUT 14
+#define HLT 15
+
+#define NOP 30
+#define RUN 31
+
+
+const static uint8_t programs[EEPROM_SIZE / RAM_SIZE][RAM_SIZE] PROGMEM = {
+  {
+    LDA, 9,
+    ADD, 10,
+    STA, 9,
+    OUT,
+    JMP, 0,
+    0,  //  9
+    1,  // 10
+  },
+  {
+    LDP, 25, // address
+    OUT,
+    SUB, 25, // address
+    JZ, 7,
+    HLT,
+    LDA, 25, // address
+    ADI, 1,
+    JC, 18, // jump to reset if address overflow
+    STA, 25, // address
+    JMP, 0, 
+    LDI, 24, // reset address to end of program
+    STA, 25, // address
+    JMP, 0,
+    25, // address
+    25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+    40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+    60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
+    80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
+    100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119,
+    120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139,
+    140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159,
+    160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179,
+    180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199,
+    200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219,
+    220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239,
+    240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255,
+  },
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+};
+
+
+/*
+ * Output the address bits and outputEnable signal using shift registers.
+ */
+void setAddress(int address, bool outputEnable) {
+  shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, (address >> 8) | (outputEnable ? 0x00 : 0x80));
+  shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, address);
+
+  digitalWrite(SHIFT_LATCH, LOW);
+  digitalWrite(SHIFT_LATCH, HIGH);
+  digitalWrite(SHIFT_LATCH, LOW);
+}
+
+
+/*
+ * Read a byte from the EEPROM at the specified address.
+ */
+byte readEEPROM(int address) {
+  for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin += 1) {
+    pinMode(pin, INPUT);
+  }
+  setAddress(address, /*outputEnable*/ true);
+
+  byte data = 0;
+  for (int pin = EEPROM_D7; pin >= EEPROM_D0; pin -= 1) {
+    data = (data << 1) + digitalRead(pin);
+  }
+  return data;
+}
+
+
+/*
+ * Write a byte to the EEPROM at the specified address.
+ */
+void writeEEPROM(int address, byte data) {
+  setAddress(address, /*outputEnable*/ false);
+  for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin += 1) {
+    pinMode(pin, OUTPUT);
+  }
+
+  for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin += 1) {
+    digitalWrite(pin, data & 1);
+    data = data >> 1;
+  }
+  digitalWrite(WRITE_EN, LOW);
+  delayMicroseconds(1);
+  digitalWrite(WRITE_EN, HIGH);
+  delay(5);
+}
+
+
+/*
+ * Read the contents of the EEPROM and print them to the serial monitor.
+ */
+void printContents(int start, int length) {
+  for (int base = start; base < length; base += 16) {
+    byte data[16];
+    for (int offset = 0; offset <= 15; offset += 1) {
+      data[offset] = readEEPROM(base + offset);
+    }
+
+    char buf[80];
+    sprintf(buf, "%04x:  %02x %02x %02x %02x %02x %02x %02x %02x   %02x %02x %02x %02x %02x %02x %02x %02x",
+            base, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
+            data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]);
+
+    Serial.println(buf);
+  }
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(SHIFT_DATA, OUTPUT);
+  pinMode(SHIFT_CLK, OUTPUT);
+  pinMode(SHIFT_LATCH, OUTPUT);
+  digitalWrite(WRITE_EN, HIGH);
+  pinMode(WRITE_EN, OUTPUT);
+  Serial.begin(57600);
+
+  // Program data bytes
+  Serial.print("Programming EEPROM");
+
+  // Write the programs into the EEPROM
+  for (int address = 0; address < EEPROM_SIZE; address += 1) {
+    int program = address >> 8;
+    int instruction = address & 0b11111111;
+
+    writeEEPROM(address, pgm_read_byte(&programs[program][instruction]));
+
+    if (address % 64 == 0) {
+      Serial.print(".");
+    }
+  }
+
+  Serial.println(" done");
+
+  // Read and print out the contents of the EERPROM
+  Serial.println("Reading EEPROM");
+  printContents(0, EEPROM_SIZE);
+}
+
+
+void loop() {
+}
---
+++ microcode-with-flags/microcode-with-flags.ino
@@ -0,0 +1,190 @@
+/**
+ * This sketch programs the microcode EEPROMs for the 8-bit breadboard computer
+ * It includes support for a flags register with carry and zero flags
+ * See this video for more: https://youtu.be/Zg1NdPKoosU
+ */
+#define SHIFT_DATA 2
+#define SHIFT_CLK 3
+#define SHIFT_LATCH 4
+#define EEPROM_D0 5
+#define EEPROM_D7 12
+#define WRITE_EN 13
+
+#define HLT 0b10000000000000000000000000000000  // Halt clock
+#define MI  0b01000000000000000000000000000000  // Memory address register in
+#define RI  0b00100000000000000000000000000000  // RAM data in
+#define RO  0b00010000000000000000000000000000  // RAM data out
+#define TR  0b00001000000000000000000000000000  // T-state reset
+#define II  0b00000100000000000000000000000000  // Instruction register in
+#define AI  0b00000010000000000000000000000000  // A register in
+#define AO  0b00000001000000000000000000000000  // A register out
+#define EO  0b00000000100000000000000000000000  // ALU out
+#define SU  0b00000000010000000000000000000000  // ALU subtract
+#define BI  0b00000000001000000000000000000000  // B register in
+#define OI  0b00000000000100000000000000000000  // Output register in
+#define CE  0b00000000000010000000000000000000  // Program counter enable
+#define CO  0b00000000000001000000000000000000  // Program counter out
+#define J   0b00000000000000100000000000000000  // Jump (program counter in)
+#define FI  0b00000000000000010000000000000000  // Flags in
+#define JC  0b00000000000000001000000000000000  // JC
+#define JZ  0b00000000000000000100000000000000  // JZ
+#define BO  0b00000000000000000010000000000000  // B register out
+#define LI  0b00000000000000000001000000000000  // Bootloader address register in
+#define LO  0b00000000000000000000100000000000  // Bootloader data out
+
+
+const static uint32_t FETCH_PC = CO|MI;
+const static uint32_t FETCH_INS = RO|II|CE;
+const static uint32_t FETCH_ADDR = CO|MI;
+
+const static uint32_t ucode[32][16] PROGMEM = {
+  { CO|LI|MI|OI,  LO|RI,      CO|II,        CE,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 00000 - BOOT
+  { FETCH_PC,     FETCH_INS,  FETCH_ADDR,   RO|MI|CE,  RO|AI,  TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 00001 - LDA
+  { FETCH_PC,     FETCH_INS,  FETCH_ADDR,   RO|MI|CE,  RO|BI,  EO|AI|FI,     TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 00010 - ADD
+  { FETCH_PC,     FETCH_INS,  FETCH_ADDR,   RO|MI|CE,  RO|BI,  EO|AI|SU|FI,  TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 00011 - SUB
+  { FETCH_PC,     FETCH_INS,  FETCH_ADDR,   RO|MI|CE,  AO|RI,  TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 00100 - STA
+  { FETCH_PC,     FETCH_INS,  FETCH_ADDR,   RO|AI|CE,  TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 00101 - LDI
+  { FETCH_PC,     FETCH_INS,  FETCH_ADDR,   RO|J,      TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 00110 - JMP
+  { FETCH_PC,     FETCH_INS,  FETCH_ADDR,   RO|JC|CE,  TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 00111 - JC
+  { FETCH_PC,     FETCH_INS,  FETCH_ADDR,   RO|JZ|CE,  TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 01000 - JZ
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 01001 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 01010 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 01011 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 01100 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 01101 - (NOP)
+  { FETCH_PC,     FETCH_INS,  AO|OI,        TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 01110 - OUT
+  { FETCH_PC,     FETCH_INS,  HLT,          TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 01111 - HLT
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 10000 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 10001 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 10010 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 10011 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 10100 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 10101 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 10110 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 10111 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 11000 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 11001 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 11010 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 11011 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 11100 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 11101 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           TR,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 11110 - (NOP)
+  { FETCH_PC,     FETCH_INS,  TR,           CE,        TR,     TR,           TR, TR, TR, TR, TR, TR, TR, TR, TR, TR },   // 11111 - RUN
+};
+
+/*
+ * Output the address bits and outputEnable signal using shift registers.
+ */
+void setAddress(int address, bool outputEnable) {
+  shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, (address >> 8) | (outputEnable ? 0x00 : 0x80));
+  shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, address);
+
+  digitalWrite(SHIFT_LATCH, LOW);
+  digitalWrite(SHIFT_LATCH, HIGH);
+  digitalWrite(SHIFT_LATCH, LOW);
+}
+
+
+/*
+ * Read a byte from the EEPROM at the specified address.
+ */
+byte readEEPROM(int address) {
+  for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin += 1) {
+    pinMode(pin, INPUT);
+  }
+  setAddress(address, /*outputEnable*/ true);
+
+  byte data = 0;
+  for (int pin = EEPROM_D7; pin >= EEPROM_D0; pin -= 1) {
+    data = (data << 1) + digitalRead(pin);
+  }
+  return data;
+}
+
+
+/*
+ * Write a byte to the EEPROM at the specified address.
+ */
+void writeEEPROM(int address, byte data) {
+  setAddress(address, /*outputEnable*/ false);
+  for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin += 1) {
+    pinMode(pin, OUTPUT);
+  }
+
+  for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin += 1) {
+    digitalWrite(pin, data & 1);
+    data = data >> 1;
+  }
+  digitalWrite(WRITE_EN, LOW);
+  delayMicroseconds(1);
+  digitalWrite(WRITE_EN, HIGH);
+  delay(10);
+}
+
+
+/*
+ * Read the contents of the EEPROM and print them to the serial monitor.
+ */
+void printContents(int start, int length) {
+  for (int base = start; base < length; base += 16) {
+    byte data[16];
+    for (int offset = 0; offset <= 15; offset += 1) {
+      data[offset] = readEEPROM(base + offset);
+    }
+
+    char buf[80];
+    sprintf(buf, "%04x:  %02x %02x %02x %02x %02x %02x %02x %02x   %02x %02x %02x %02x %02x %02x %02x %02x",
+            base, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7],
+            data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15]);
+
+    Serial.println(buf);
+  }
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(SHIFT_DATA, OUTPUT);
+  pinMode(SHIFT_CLK, OUTPUT);
+  pinMode(SHIFT_LATCH, OUTPUT);
+  digitalWrite(WRITE_EN, HIGH);
+  pinMode(WRITE_EN, OUTPUT);
+  Serial.begin(57600);
+
+  // Program data bytes
+  Serial.print("Programming EEPROM");
+
+  // Program the 8 high-order bits of microcode into the first 2048 bytes of EEPROM
+  for (int address = 0; address < 2048; address += 1) {
+    int bit_select_3 = (address & 0b10000000000) >> 10;
+    int bit_select_2 = (address & 0b01000000000) >> 9;
+    int instruction  = (address & 0b00111110000) >> 4;
+    int step         = (address & 0b00000001111);
+
+    if (bit_select_3) {
+      writeEEPROM(address, pgm_read_dword(&ucode[instruction][step]) >> 8);
+    } else if (bit_select_2) {
+      writeEEPROM(address, pgm_read_dword(&ucode[instruction][step]) >> 16);
+    } else {
+      writeEEPROM(address, pgm_read_dword(&ucode[instruction][step]) >> 24);
+    }
+
+    if (address % 64 == 0) {
+      Serial.print(".");
+    }
+  }
+
+  Serial.println(" done");
+
+
+  // Read and print out the contents of the EERPROM
+  Serial.println("Reading EEPROM");
+  printContents(0, 2048);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
